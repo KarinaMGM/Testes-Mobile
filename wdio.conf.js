eec64c97a21d4b23d50d8a59987289791aeb9174
Karina Macedo Gonçalves Moura <140670360+KarinaMGM@users.noreply.github.com>
2024-03-11 15:31:17 -0300

    sauce labs

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,7 +1,14 @@
 export const config = {
    
-    runner: 'local',
-    port: 4723,
+  //  runner: 'local',
+  //  port: 4723,
+    
+        user: 'karinamgm',
+        key: 'fd833928-57dd-4ea5-bed0-a26b7a03b540',
+        hostname: 'ondemand.us-west-1.saucelabs.com',
+        port: 443,
+        baseUrl: 'wd/hub',
+
     specs: [
         './test/specs/**/*.js'
     ],
@@ -9,7 +16,8 @@ export const config = {
     ],
     maxInstances: 10,
         
-    capabilities: [{
+    capabilities: [
+/*        {
         platformName: 'Android',
         'appium:deviceName': 'ebac-mobile2',
         'appium:platformVersion': '9.0',
@@ -17,7 +25,24 @@ export const config = {
         'appium:app': `${process.cwd()}/app/ebacshop.apks`,
         'appium:appWaitActivity': '.MainActivity',
         'appim:disableIdLocatorAutocompletion': true
-    }],
+    }
+*/
+
+{
+    platformName: 'Android',
+    'appium:app': 'storage:filename=ebacshop.aab', 
+    'appium:deviceName': 'Samsung.*',
+    'appium:platformVersion': '10',
+    'appium:automationName': 'UiAutomator2',
+    'appim:disableIdLocatorAutocompletion': true,
+    'sauce:options': {
+       build: 'appium-build-tarefa-modulo17',
+       name: 'tarefa módulo 17',
+       deviceOrientation: 'PORTRAIT',
+       appiumVersion: '2.0.0',
+     },
+   }],
+
 
     logLevel: 'info',
     bail: 0,
